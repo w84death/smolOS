29713847a4815dd29b70161b27da9287e38793c6
Krzysztof Jankowski <kj@p1x.in>
2023-07-24 16:19:30 +0200

    Update plasma.py

--- plasma.py
+++ plasma.py
@@ -1,4 +1,7 @@
-# Plasma effect for Adafruit NeoPixel BFF 5x5 LED Grid
+"""
+Plasma effect for Adafruit NeoPixel BFF 5x5 LED Grid
+(c)2023/07 Krzysztof Krystian Jankowski
+"""
 import machine
 import utime
 import _thread
@@ -7,15 +10,27 @@ import neopixel
 import math
 import random
 
-class neo_plasma():
-    def __init__(self):
-        self.thread_running = False
+# Define constants
+LED_PIN = 29
+LED_COUNT = 5*5
+PLASMA_SPEED = 0.05
+ZOOM = 0.33
+BRIGHTNESS_POW = 0.1
 
-        self.pixels = np = neopixel.NeoPixel(machine.Pin(29),5*5)
+# Define neo_plasma class
+class NeoPlasma:
+    """
+    A class to handle the NeoPlasma functionalities.
+    """
+    def __init__(self):
+        """
+        Initialize the NeoPlasma object.
+        """
+        self.thread_running = False
+        self.pixels = np = neopixel.NeoPixel(machine.Pin(LED_PIN), LED_COUNT)
         self.pixels.fill((0,0,0))
         self.pixels.write()
-        self.brightness = 0.2
-
+        self.brightness = BRIGHTNESS_POW
         self.hearth_bitmap = [
             0,1,1,0,0,
             1,1,1,1,0,
@@ -24,38 +39,57 @@ class neo_plasma():
             0,1,1,0,0,
         ]
 
+    def calculate_color(self, time, p):
+        """
+        Calculate color for the pixel.
+        """
+        x = p%5-2
+        y = p/5-2
+        c = math.sin(math.sin(x*ZOOM+math.cos(time*0.3)*13) + math.cos(y*ZOOM+math.sin(time*.2)*17))
+        c = (128+int(c*128))*BRIGHTNESS_POW
+        if self.hearth_bitmap[24-p] == 0:
+            c = c*0.025
+        return (
+            int(c+5+math.sin(time*0.21)*5),
+            int(c+5+math.cos(1+time*0.33)*5),
+            int(c+5+math.sin(1+time*0.47)*5)
+        )
+
     def plasma(self):
-        time=0
-        zoom=0.33
-        pow=0.1
+        """
+        Create a plasma effect.
+        """
+        time = 0
         while self.thread_running:
-            for p in range(25):
-                x=p%5-2
-                y=p/5-2
-                c=math.sin(math.sin(x*zoom+math.cos(time*0.3)*13) + math.cos(y*zoom+math.sin(time*.2)*17))
-                c=(128+int(c*128))*pow
-                if self.hearth_bitmap[24-p]==0:
-                    c=c*0.025
-                self.pixels[p]=(
-                    int(c+5+math.sin(time*0.21)*5),
-                    int(c+5+math.cos                                                                                                (1+time*0.33)*5),
-                    int(c+5+math.sin(1+time*0.47)*5))
+            for p in range(LED_COUNT):
+                self.pixels[p] = self.calculate_color(time, p)
             self.pixels.write()
-            time+=0.05
+            time += PLASMA_SPEED
 
     def demo(self):
+        """
+        Demo the plasma effect.
+        """
         self.start(self.plasma)
 
     def stop(self):
+        """
+        Stop the plasma effect.
+        """
         self.thread_running = False
         print("NeoPixel: Thread stopped. Use plasma.demo()")
 
-    def start(self,fn):
+    def start(self, fn):
+        """
+        Start the plasma effect.
+        """
         if not self.thread_running:
             self.thread_running = True
-            _thread.start_new_thread(fn,())
+            _thread.start_new_thread(fn, ())
             print("NeoPixel: Demo thread started in background. Use plasma.stop()")
         else:
             print("NeoPixel: Thread already used. Use plasma.stop()")
 
-#plasma = neo_plasma()
+# To use this refactored code, you would do something like the following:
+# plasma = NeoPlasma()
+# plasma.demo()
