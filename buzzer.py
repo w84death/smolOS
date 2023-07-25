870a89a16df7e06b8e27ec68f6f9ff5ee2741837
Krzysztof Jankowski <kj@p1x.in>
2023-07-25 11:43:24 +0200

    Update buzzer.py

--- buzzer.py
+++ buzzer.py
@@ -2,8 +2,29 @@ from machine import Pin, PWM
 import time
 import _thread
 
-class Buzz():
-    def __init__(self,pin=3):
+# Define constants
+BUZZER_PIN = 3
+BUZZER_DUTY = 10000
+NOTE_DURATION = 0.1
+REPEAT = 4
+SONGS = [
+    ['C', 'D', 'E', 'D', 'C', 'E', 'C', 'A', 'A', 'G', 'A', 'G', 'E', 'C', 'E', 'D'],
+    ['C', 'E', 'G', 'E', 'A', 'F', 'D', 'F', 'C', 'D', 'F', 'D', 'G', 'E', 'C', 'E'],
+    ['E', 'G', 'B', 'G', 'C', 'A', 'F', 'A', 'E', 'F', 'A', 'F', 'B', 'G', 'E', 'G'],
+    ['G', 'B', 'D', 'B', 'E', 'C', 'A', 'C', 'G', 'A', 'C', 'A', 'D', 'B', 'G', 'B'],
+    ['C', 'E', 'G', 'E', 'C', 'A', 'F', 'A', 'C', 'G', 'E', 'G', 'C', 'F', 'D', 'F', 'A', 'F', 'D', 'F', 'A', 'G', 'E', 'G', 'A', 'F', 'D', 'F', 'A', 'C', 'A', 'G'],
+    ['E', 'G', 'B', 'G', 'E', 'C', 'A', 'C', 'E', 'D', 'F', 'D', 'E', 'G', 'B', 'G', 'C', 'A', 'F', 'A', 'C', 'B', 'G', 'B', 'C', 'A', 'F', 'A', 'C', 'D', 'C', 'B'],
+    ['G', 'B', 'D', 'B', 'G', 'E', 'C', 'E', 'G', 'F', 'A', 'F', 'G', 'B', 'D', 'B', 'E', 'C', 'A', 'C', 'E', 'D', 'B', 'D', 'E', 'C', 'A', 'C', 'E', 'F', 'E', 'D']
+]
+
+class Buzz:
+    """
+    A class to handle the Buzz functionalities.
+    """
+    def __init__(self):
+        """
+        Initialize the Buzz object.
+        """
         self.thread_running = False
         self.notes = {
             'C': 261,
@@ -14,82 +35,81 @@ class Buzz():
             'A': 440,
             'B': 494
         }
-
-        self.buzzer = PWM(Pin(pin, Pin.OUT))
-        self.play_note('C',0.2)
+        self.buzzer = PWM(Pin(BUZZER_PIN, Pin.OUT))
+        self.play_note('C', 0.2)
         print("smolBuzzer: buzz.play_note('A',0.2), buzz.play_demo(), buzz.stop().")
         
-    def play_note(self,note,duration):
+    def play_note(self, note, duration):
+        """
+        Play a specific note for a certain duration.
+        """
         if note != ' ':
             self.buzzer.freq(int(self.notes[note]*0.5))
-            self.buzzer.duty_u16(10000)
+            self.buzzer.duty_u16(BUZZER_DUTY)
             time.sleep(0.05) 
-            self.buzzer.freq(self.notes[note])  # set the buzzer frequency
-            self.buzzer.duty_u16(10000)            
+            self.buzzer.freq(self.notes[note])
+            self.buzzer.duty_u16(BUZZER_DUTY)
         else:
             self.buzzer.duty_u16(0)
-
-        time.sleep(duration)      # play the note for the desired duration
+        time.sleep(duration)
         self.buzzer.freq(int(self.notes[note]*0.5))
-        self.buzzer.duty_u16(10000)
+        self.buzzer.duty_u16(BUZZER_DUTY)
         time.sleep(0.05)
-        
-        self.buzzer.duty_u16(0) 
-
+        self.buzzer.duty_u16(0)
+    
     def demo_thread(self):
-        song0 = ['C', 'D', 'E', 'D', 'C', 'E', 'C', 'A', 'A', 'G', 'A', 'G', 'E', 'C', 'E', 'D']
-        song1 = ['C', 'E', 'G', 'E', 'A', 'F', 'D', 'F', 'C', 'D', 'F', 'D', 'G', 'E', 'C', 'E']
-        song2 = ['E', 'G', 'B', 'G', 'C', 'A', 'F', 'A', 'E', 'F', 'A', 'F', 'B', 'G', 'E', 'G']
-        song3 = ['G', 'B', 'D', 'B', 'E', 'C', 'A', 'C', 'G', 'A', 'C', 'A', 'D', 'B', 'G', 'B']
-        song4 = ['C', 'E', 'G', 'E', 'C', 'A', 'F', 'A', 'C', 'G', 'E', 'G', 'C', 'F', 'D', 'F', 'A', 'F', 'D', 'F', 'A', 'G', 'E', 'G', 'A', 'F', 'D', 'F', 'A', 'C', 'A', 'G']
-        song5 = ['E', 'G', 'B', 'G', 'E', 'C', 'A', 'C', 'E', 'D', 'F', 'D', 'E', 'G', 'B', 'G', 'C', 'A', 'F', 'A', 'C', 'B', 'G', 'B', 'C', 'A', 'F', 'A', 'C', 'D', 'C', 'B']
-        song6 = ['G', 'B', 'D', 'B', 'G', 'E', 'C', 'E', 'G', 'F', 'A', 'F', 'G', 'B', 'D', 'B', 'E', 'C', 'A', 'C', 'E', 'D', 'B', 'D', 'E', 'C', 'A', 'C', 'E', 'F', 'E', 'D']
-
-
-        playlist = [song0,song1,song2,song3,song4,song5,song6]
-        note_duration = 0.1
-        repeat = 4
-        
+        """
+        Play a demo of songs in a new thread.
+        """
         while self.thread_running:
             no=0
-            for song in playlist:
+            for song in SONGS:
                 if not self.thread_running:
-                        return
+                    return
                 print("smoBuzzer: Track",no)
-                for prog in range(repeat):
+                for prog in range(REPEAT):
                     progress = "#"
                     for _ in range(prog):
                         progress += "#"
-                    for _ in range(repeat-prog-1):
+                    for _ in range(REPEAT-prog-1):
                         progress += "_"
-                        
                     print("\t["+progress+"]")
                     for note in song:
                         if not self.thread_running:
                             return
-                        self.play_note(note, note_duration)
+                        self.play_note(note, NOTE_DURATION)
                 time.sleep(2)
                 no+=1
     
-    def start_fg(self):        
+    def start_unthreaded(self):        
+        """
+        Start the demo in the current thread.
+        """
         self.thread_running = True
         self.demo_thread()
-
-    def start(self, threaded=True):
-        if threaded:
-            if not self.thread_running:
-                self.thread_running = True
-                _thread.start_new_thread(self.demo_thread,())
-                print("smoBuzzer: Playing demo track...")
-            else:
-                print("smoBuzzer: Thread already in use. Kill other backround programs.")
-        else:
+        
+    def start_threaded(self):
+        """
+        Start the demo in a new thread.
+        """
+        if not self.thread_running:
             self.thread_running = True
-            self.demo_thread()
+            _thread.start_new_thread(self.demo_thread,())
+            print("smoBuzzer: Playing demo track...")
+        else:
+            print("smoBuzzer: Thread already in use. Kill other backround programs.")
             
     def stop(self):
+        """
+        Stop the demo.
+        """
         self.thread_running = False
         time.sleep(0.1) 
         self.buzzer.duty_u16(0)
 
-#buzz  = Buzz()
+# To use this refactored code, you would do something like the following:
+# buzz = Buzz()
+# buzz.start_threaded()  # to start the demo in a new thread
+# or
+# buzz.start_unthreaded()  # to start the demo in the current thread
+# buzz.stop() to stop the demo
