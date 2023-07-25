9861a6467a01a5928505868ff95c15f6109ba628
Krzysztof Jankowski <kj@p1x.in>
2023-07-25 11:02:03 +0200

    Update template.py

--- template.py
+++ template.py
@@ -1,43 +1,69 @@
-# Template for user programs for smolOS
-#
+"""
+Template for user programs for smolOS
+"""
 
 import time
 import _thread
 
-class Template():
+# Define constants
+MESSAGE_INTERVAL = 10
+
+class Template:
+    """
+    A template for user programs for smolOS.
+    """
     def __init__(self):
+        """
+        Initialize the Template object.
+        """
         self.name = "Template Program"
         self.thread_running = False
-        
         self.msg("Program initialized")
     
     def loop(self):
-        # your code goes here..
-        # sample loop
+        """
+        Loop where the main logic of the program goes.
+        """
         t = 0
         while self.thread_running:
-            if t%10==0:
-                self.msg(str(t)+"th!")
+            if t % MESSAGE_INTERVAL == 0:
+                self.msg(f"{t}th!")
             time.sleep(1)
             t += 1
             
-    def msg(self,msg):
-        print(self.name,":",msg)
+    def msg(self, message):
+        """
+        Print a message from the program.
+        """
+        print(f"{self.name} : {message}")
         
     def stop(self):
+        """
+        Stop the program.
+        """
         self.thread_running = False
         self.msg("Program stopped. Bye!")
-
-    def start(self, threaded=True):
-        if threaded:
-            if not self.thread_running:
-                self.thread_running = True
-                _thread.start_new_thread(self.loop,())
-                self.msg("Program loop started in background.")
-            else:
-                self.msg("Thread is busy. Stop program in background.")
-        else:
+        
+    def start_threaded(self):
+        """
+        Start the program in a new thread.
+        """
+        if not self.thread_running:
             self.thread_running = True
-            self.loop()
-            
-template = Template()
+            _thread.start_new_thread(self.loop, ())
+            self.msg("Program loop started in background.")
+        else:
+            self.msg("Thread is busy. Stop program in background.")
+        
+    def start_unthreaded(self):
+        """
+        Run the program in the current thread.
+        """
+        self.thread_running = True
+        self.loop()
+
+# To use this refactored code, you would do something like the following:
+# template = Template()
+# template.start_threaded()  # to start in a new thread
+# or
+# template.start_unthreaded()  # to run in the current thread
