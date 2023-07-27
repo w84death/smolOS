707d4e413fcbfabfd99aad876bad5308e240b4d9
Krzysztof Krystian Jankowski <kj@p1x.in>
2023-07-28 01:12:03 +0200

    smal update to main

--- main.py
+++ main.py
@@ -6,7 +6,16 @@ Specialized Microcontroller-Oriented Lightweight Operating System
 (c)2023/07 Krzysztof Krystian Jankowski
 Homepage: https://smol.p1x.in/os/
 """
-
+import machine
+import utime
 from smolos import smolOS
+
 os=smolOS()
-os.boot()
+
+try:
+    os.boot()
+    print("Bye!")
+except OSError:
+    print("Oh, no!\n")
+    utime.sleep(1)
+    machine.soft_reset()
