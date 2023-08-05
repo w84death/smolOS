11b98f3929a1b8d8a2c381958badb70dcf9f7838
Krzysztof Krystian Jankowski <kj@p1x.in>
2023-08-05 22:53:19 +0200

    bytebeat updated

--- bytebeat.py
+++ bytebeat.py
@@ -12,8 +12,9 @@ import neopixel
 
 sin = math.sin
 BUZZER_PIN = 4
-BUZZER_DUTY = 8000
-PI  =  3.1415926535
+BUZZER_DUTY = 32768
+# For a buzzer, you typically want a 50% duty cycle (65535), which will produce a square wave. This will give you the most balanced and loudest sound for most buzzers.
+PI = 3.1415926535
 PI2 = PI*2
 NEOPIXEL_PIN = 12
 NEOPIXELS_PIN = 29
@@ -21,8 +22,7 @@ NEOPIXELS_PIN = 29
 class ByteBeat:
 
     def __init__(self):
-        self.t = 0
-        machine.freq(260000000) # 260MHz
+        self.t = 512
         self.pixel = neopixel.NeoPixel(machine.Pin(NEOPIXEL_PIN), 1)
         self.power = machine.Pin(NEOPIXEL_PIN-1, machine.Pin.OUT)
         self.power.value(1)
@@ -35,16 +35,16 @@ class ByteBeat:
     # bytebeat formula
     def bytebeat(self, t):
         return [
-            int((127+128*sin((t>>8)+10.9714)*PI2)*.1),
-            int((127+128*sin((1.14286*(t>>13)+10.9714+(0.05*sin(t>>13)))*PI2+1+8*sin(t>>8)*0.01))),
-            int((15+16*sin((4.8*(t>>12)+3.36))/32))]
+            int((127+128*sin(0.285714*(t&t>>8)+7.71429)*PI2)*.01),
+            int((127+128*sin((1.11905*(t>>10)+7.71429+(2*sin(t>>8)))*PI2))),
+            int(31+32*sin((4.8*(t>>11)*PI)))>>4]
 
     def start(self):
         while True:
             try:
                 bb=self.bytebeat(self.t)
-                bites = 32 + (bb[0] & bb[1] | bb[2])*2
-                self.buzzer.freq(bites)
+                bites = bb[0] | bb[1] | bb[2]
+                self.buzzer.freq(261+bites%255)
                 self.t += 1
 
                 color = (int(bb[0]%255),int(bb[1]%255),int(bb[2]%255))
@@ -55,6 +55,10 @@ class ByteBeat:
 
             except KeyboardInterrupt:
                 self.buzzer.duty_u16(0)
+                self.pixels.fill((0,0,0))
+                self.pixel.fill((0,0,0))
+                self.pixels.write()
+                self.pixel.write()
                 break
 
 
